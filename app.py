a8b0377389a0c0a94c7a72fe1edb72a6d7af1c16
Leeyeawon <wowon0528@gmail.com>
2025-12-12 16:41:09 +0900

    연결

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+app = Flask('Busan_travel')
+
+@app.route('/index')
+def indexhtml():
+    return render_template("index.html")
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == 'GET':
+        # args_dict = request.args.to_dict()
+        # print(args_dict)
+        num = request.args["num"]
+        name = request.args.get("name")
+        return "GET으로 전달된 데이터({}, {})".format(num, name)
+    else:
+        num = request.form["num"]
+        name = request.form["name"]
+        return "POST로 전달된 데이터({}, {})".format(num, name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
